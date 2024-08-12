21b07c78d30ba277ae0e27996501d2c6666c2b1c
Aryan-Sood <aryansood12@gmail.com>
2024-08-13 00:36:04 +0530

    add about page

--- src/saasservice/settings.py
+++ src/saasservice/settings.py
@@ -25,7 +25,11 @@ SECRET_KEY = 'django-insecure-2%%6ol^rf@*30tjh^9)sbo9ycr#487n-%66ufdmd%_10g0efg^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    ".railway.app"  # https://saas.prod.railway.app
+]
+if DEBUG:
+    ALLOWED_HOSTS += ['127.0.0.1','localhost']
 
 
 # Application definition
--- src/saasservice/urls.py
+++ src/saasservice/urls.py
@@ -1,9 +1,12 @@
 
 from django.contrib import admin
 from django.urls import path
-from .views import home_page_view
+from .views import home_view, about_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',home_page_view)
+    path('hello-world', home_view),
+    path('hello-world.html',home_view),
+    path('',home_view), # root page
+    path('about/',about_view)
 ]
