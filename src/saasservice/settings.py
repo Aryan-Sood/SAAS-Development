a1af0593165cea65c82f5dc888f32497967ab8e8
Aryan-Sood <aryansood12@gmail.com>
2024-08-13 16:29:35 +0530

    set up local cdn

--- src/saasservice/settings.py
+++ src/saasservice/settings.py
@@ -26,8 +26,9 @@ SECRET_KEY = config("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
-DEBUG = config("DJANGO_DEBUG", cast = bool)
-print("DEBUG: ", DEBUG, type(DEBUG))
+# DEBUG = config("DJANGO_DEBUG", cast = bool)
+# print("DEBUG: ", DEBUG, type(DEBUG))
+DEBUG = True
 
 ALLOWED_HOSTS = [
     ".railway.app"  # https://saas.prod.railway.app
@@ -134,6 +135,17 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_BASE_DIR = BASE_DIR/"staticfiles"
+STATICFILES_VENDOR_DIR = STATICFILES_BASE_DIR/"vendors"
+
+# sources
+STATICFILESDIR = [
+    STATICFILES_BASE_DIR
+]
+
+# output
+# local cdn
+STATIC_ROOT = BASE_DIR.parent/'local-cdn'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
